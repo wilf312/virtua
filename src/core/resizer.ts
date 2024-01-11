fb1aa2a23b02bee81e53ec16cd541d07b7c70b8e
OkadaGenya <g.okada.wilf+github@gmail.com>
2024-01-11 11:46:47 +0900

    fix Error: ResizeObserver loop limit exceeded

--- src/core/resizer.ts
+++ src/core/resizer.ts
@@ -32,24 +32,26 @@ export const createResizer = (
   const getResizeObserver = once(() => {
     // https://www.w3.org/TR/resize-observer/#intro
     return new ResizeObserver((entries) => {
-      const resizes: ItemResize[] = [];
-      for (const { target, contentRect } of entries) {
-        // Skip zero-sized rects that may be observed under `display: none` style
-        if (!(target as HTMLElement).offsetParent) continue;
+      setTimeout(() => {
+        const resizes: ItemResize[] = [];
+        for (const { target, contentRect } of entries) {
+          // Skip zero-sized rects that may be observed under `display: none` style
+          if (!(target as HTMLElement).offsetParent) continue;
 
-        if (target === viewportElement) {
-          store._update(ACTION_VIEWPORT_RESIZE, contentRect[sizeKey]);
-        } else {
-          const index = mountedIndexes.get(target);
-          if (exists(index)) {
-            resizes.push([index, contentRect[sizeKey]]);
+          if (target === viewportElement) {
+            store._update(ACTION_VIEWPORT_RESIZE, contentRect[sizeKey]);
+          } else {
+            const index = mountedIndexes.get(target);
+            if (exists(index)) {
+              resizes.push([index, contentRect[sizeKey]]);
+            }
           }
         }
-      }
 
-      if (resizes.length) {
-        store._update(ACTION_ITEM_RESIZE, resizes);
-      }
+        if (resizes.length) {
+          store._update(ACTION_ITEM_RESIZE, resizes);
+        }
+      }, 0);
     });
   });
 
@@ -93,20 +95,22 @@ export const createWindowResizer = (
   const getResizeObserver = once(() => {
     // https://www.w3.org/TR/resize-observer/#intro
     return new ResizeObserver((entries) => {
-      const resizes: ItemResize[] = [];
-      for (const { target, contentRect } of entries) {
-        // Skip zero-sized rects that may be observed under `display: none` style
-        if (!(target as HTMLElement).offsetParent) continue;
+      setTimeout(() => {
+        const resizes: ItemResize[] = [];
+        for (const { target, contentRect } of entries) {
+          // Skip zero-sized rects that may be observed under `display: none` style
+          if (!(target as HTMLElement).offsetParent) continue;
 
-        const index = mountedIndexes.get(target);
-        if (exists(index)) {
-          resizes.push([index, contentRect[sizeKey]]);
+          const index = mountedIndexes.get(target);
+          if (exists(index)) {
+            resizes.push([index, contentRect[sizeKey]]);
+          }
         }
-      }
 
-      if (resizes.length) {
-        store._update(ACTION_ITEM_RESIZE, resizes);
-      }
+        if (resizes.length) {
+          store._update(ACTION_ITEM_RESIZE, resizes);
+        }
+      }, 0);
     });
   });
   const onWindowResize = () => {
@@ -161,82 +165,84 @@ export const createGridResizer = (
   const getResizeObserver = once(() => {
     // https://www.w3.org/TR/resize-observer/#intro
     return new ResizeObserver((entries) => {
-      const resizedRows = new Set<number>();
-      const resizedCols = new Set<number>();
-      for (const { target, contentRect } of entries) {
-        // Skip zero-sized rects that may be observed under `display: none` style
-        if (!(target as HTMLElement).offsetParent) continue;
+      setTimeout(() => {
+        const resizedRows = new Set<number>();
+        const resizedCols = new Set<number>();
+        for (const { target, contentRect } of entries) {
+          // Skip zero-sized rects that may be observed under `display: none` style
+          if (!(target as HTMLElement).offsetParent) continue;
 
-        if (target === viewportElement) {
-          vStore._update(ACTION_VIEWPORT_RESIZE, contentRect[heightKey]);
-          hStore._update(ACTION_VIEWPORT_RESIZE, contentRect[widthKey]);
-        } else {
-          const cell = mountedIndexes.get(target);
-          if (cell) {
-            const [rowIndex, colIndex] = cell;
-            const key = getKey(rowIndex, colIndex);
-            const prevSize = sizeCache.get(key);
-            const size: CellSize = [
-              contentRect[heightKey],
-              contentRect[widthKey],
-            ];
-            let rowResized: boolean | undefined;
-            let colResized: boolean | undefined;
-            if (!prevSize) {
-              rowResized = colResized = true;
-            } else {
-              if (prevSize[0] !== size[0]) {
-                rowResized = true;
+          if (target === viewportElement) {
+            vStore._update(ACTION_VIEWPORT_RESIZE, contentRect[heightKey]);
+            hStore._update(ACTION_VIEWPORT_RESIZE, contentRect[widthKey]);
+          } else {
+            const cell = mountedIndexes.get(target);
+            if (cell) {
+              const [rowIndex, colIndex] = cell;
+              const key = getKey(rowIndex, colIndex);
+              const prevSize = sizeCache.get(key);
+              const size: CellSize = [
+                contentRect[heightKey],
+                contentRect[widthKey],
+              ];
+              let rowResized: boolean | undefined;
+              let colResized: boolean | undefined;
+              if (!prevSize) {
+                rowResized = colResized = true;
+              } else {
+                if (prevSize[0] !== size[0]) {
+                  rowResized = true;
+                }
+                if (prevSize[1] !== size[1]) {
+                  colResized = true;
+                }
               }
-              if (prevSize[1] !== size[1]) {
-                colResized = true;
+              if (rowResized) {
+                resizedRows.add(rowIndex);
               }
-            }
-            if (rowResized) {
-              resizedRows.add(rowIndex);
-            }
-            if (colResized) {
-              resizedCols.add(colIndex);
-            }
-            if (rowResized || colResized) {
-              sizeCache.set(key, size);
+              if (colResized) {
+                resizedCols.add(colIndex);
+              }
+              if (rowResized || colResized) {
+                sizeCache.set(key, size);
+              }
             }
           }
         }
-      }
 
-      if (resizedRows.size) {
-        const heightResizes: ItemResize[] = [];
-        resizedRows.forEach((rowIndex) => {
-          let maxHeight = 0;
-          maybeCachedColIndexes.forEach((colIndex) => {
-            const size = sizeCache.get(getKey(rowIndex, colIndex));
-            if (size) {
-              maxHeight = max(maxHeight, size[0]);
+        if (resizedRows.size) {
+          const heightResizes: ItemResize[] = [];
+          resizedRows.forEach((rowIndex) => {
+            let maxHeight = 0;
+            maybeCachedColIndexes.forEach((colIndex) => {
+              const size = sizeCache.get(getKey(rowIndex, colIndex));
+              if (size) {
+                maxHeight = max(maxHeight, size[0]);
+              }
+            });
+            if (maxHeight) {
+              heightResizes.push([rowIndex, maxHeight]);
             }
           });
-          if (maxHeight) {
-            heightResizes.push([rowIndex, maxHeight]);
-          }
-        });
-        vStore._update(ACTION_ITEM_RESIZE, heightResizes);
-      }
-      if (resizedCols.size) {
-        const widthResizes: ItemResize[] = [];
-        resizedCols.forEach((colIndex) => {
-          let maxWidth = 0;
-          maybeCachedRowIndexes.forEach((rowIndex) => {
-            const size = sizeCache.get(getKey(rowIndex, colIndex));
-            if (size) {
-              maxWidth = max(maxWidth, size[1]);
+          vStore._update(ACTION_ITEM_RESIZE, heightResizes);
+        }
+        if (resizedCols.size) {
+          const widthResizes: ItemResize[] = [];
+          resizedCols.forEach((colIndex) => {
+            let maxWidth = 0;
+            maybeCachedRowIndexes.forEach((rowIndex) => {
+              const size = sizeCache.get(getKey(rowIndex, colIndex));
+              if (size) {
+                maxWidth = max(maxWidth, size[1]);
+              }
+            });
+            if (maxWidth) {
+              widthResizes.push([colIndex, maxWidth]);
             }
           });
-          if (maxWidth) {
-            widthResizes.push([colIndex, maxWidth]);
-          }
-        });
-        hStore._update(ACTION_ITEM_RESIZE, widthResizes);
-      }
+          hStore._update(ACTION_ITEM_RESIZE, widthResizes);
+        }
+      }, 0);
     });
   });
 
